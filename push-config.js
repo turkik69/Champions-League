dc468eaaf32944ae146e0d61a99e21c4a9ae225a
Turki alyahyai <turki.k69@gmail.com>
2026-09-15 21:18:01 +0400

    Configure Firebase web app for FCM push

--- push-config.js
+++ push-config.js
@@ -1,14 +1,14 @@
 // Public Firebase Web Push configuration.
 // These values are public client identifiers, not server secrets.
-// push.js also checks /pushPublicConfig in Realtime Database so the public
-// identifiers can be completed without another website release.
 window.UCL_PUSH_CONFIG = {
+  apiKey: 'AIzaSyBsnryD1ZtvjzumatCCVN-QpRAMR4_IG7M',
+  authDomain: 'world-cup-2026-d3091.firebaseapp.com',
   databaseURL: 'https://world-cup-2026-d3091-default-rtdb.europe-west1.firebasedatabase.app',
   projectId: 'world-cup-2026-d3091',
-  authDomain: 'world-cup-2026-d3091.firebaseapp.com',
-  apiKey: '',
-  messagingSenderId: '',
-  appId: '',
+  storageBucket: 'world-cup-2026-d3091.firebasestorage.app',
+  messagingSenderId: '830204361101',
+  appId: '1:830204361101:web:f3a23c0fa41bb809d365c4',
+  measurementId: 'G-4Y1R6PW3SL',
   vapidKey: '',
   publicConfigPath: 'pushPublicConfig'
 };
